adef49b89f8a06b6b2209e89586bb4e85c3ef1f5
Martiros <martiros.aghajanyan@gmail.com>
2016-04-18 20:28:44 -0700

    Validate response structure of the `GET /api/articles` endpoint

--- server/tests/article-test.js
+++ server/tests/article-test.js
@@ -1,10 +1,22 @@
 import supertest from 'supertest';
 import chai, { expect } from 'chai';
 import httpStatus from 'http-status';
+import responseAssertion from './helpers/responseAssertion';
+import Joi from 'joi';
+
+chai.use(responseAssertion);
 
 describe('Article endpoints', () => {
   let server;
   let request;
+
+  const articleSchema = Joi.object().keys({
+    id: Joi.number().integer().min(1),
+    title: Joi.string().min(10).max(200).required(),
+    content: Joi.string().min(50).max(60000).required(),
+    createdAt: Joi.date().iso().required(),
+    updatedAt: Joi.date().iso().required(),
+  });
 
   beforeEach(() => {
     delete require.cache[require.resolve('../bin/www')];
@@ -20,7 +32,14 @@ describe('Article endpoints', () => {
         .get('/api/articles')
         .expect('Content-type', /json/)
         .expect(httpStatus.OK)
-        .end(done)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          expect(res.body.items[0]).to.be.an.item(articleSchema);
+          return done();
+        })
     );
   });
 });
