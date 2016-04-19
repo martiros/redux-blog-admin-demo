65b9f9fdbe7fa89afcbfdc1cf70348f51a1a2dfd
Martiros <martiros.aghajanyan@gmail.com>
2016-04-18 20:17:41 -0700

    Add helper methods(item, collectionOfItems) to chai for response data/structure assertion
    Examples
    
    expect(res.body.item).to.be.an.item(articleSchema);
    
    expect(res.body.item).to.be.a.collectionOfItems(articleSchema);

---
+++ server/tests/helpers/responseAssertion.js
@@ -0,0 +1,55 @@
+import Joi from 'joi';
+
+export default (chai) => {
+  const { Assertion } = chai;
+
+  Assertion.addMethod('collectionOfItems', function responseContainscollectionOfItems(itemSchema) {
+    const items = this._obj;
+
+    new Assertion(items).to.be.an('array');
+
+    let validationResult = null;
+    let item = null;
+    let message = null;
+    let errorSource = '';
+    for (let i = 0; i < items.length; i++) {
+      item = items[i];
+      validationResult = Joi.validate(item, itemSchema);
+      if (validationResult.error) {
+        const { error } = validationResult;
+        errorSource = `item[${i}].${error.details[0].path}`;
+        message = error.details[0].message;
+        break;
+      }
+    }
+
+    this.assert(
+      message === null,
+      `${errorSource} doesn't pass validation.` +
+      `\nError Message - (${message})\nItem = \n${JSON.stringify(item, null, '\t')}`,
+      'expected #{this} to not be collection of items'
+    );
+  });
+
+  Assertion.addMethod('item', function responseContainsAnItem(itemSchema) {
+    const item = this._obj;
+
+    new Assertion(item).to.be.an('object');
+
+    const validationResult = Joi.validate(item, itemSchema);
+    let message = null;
+    let errorSource = '';
+    if (validationResult.error) {
+      const { error } = validationResult;
+      message = error.message;
+      errorSource = error.details[0].path;
+    }
+
+    this.assert(
+      message === null,
+      `${errorSource} doesn't pass validation.` +
+      `\nError Message - (${message})\nItem = \n${JSON.stringify(item, null, '\t')}`,
+      'expected #{this} to not be an item'
+    );
+  });
+};
