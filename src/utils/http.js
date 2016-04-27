baa628a10f7fc8863ffc7a42cab110df3d612081
Martiros <martiros.aghajanyan@gmail.com>
2016-04-26 19:52:44 -0700

    Create basic http client

---
+++ src/utils/http.js
@@ -0,0 +1,68 @@
+import request from 'superagent';
+
+export default {
+
+  /**
+   * Make HTTP request to the given URI with a GET method.
+   *
+   * @param {string} uri
+   * @param {Object} query
+   * @return {Promise}
+   */
+  get(uri, query = {}) {
+    return this.request('GET', uri, query);
+  },
+
+  /**
+   * Make HTTP request to the given URI with a POST method.
+   *
+   * @param {string} uri
+   * @param {Object} data
+   * @return {Promise}
+   */
+  post(uri, data) {
+    return this.request('POST', uri, null, data);
+  },
+
+  /**
+   * Make HTTP request to the given URI with a PUT method.
+   *
+   * @param {string} uri
+   * @param {Object} data
+   * @return {Promise}
+   */
+  put(uri, data) {
+    return this.request('PUT', uri, null, data);
+  },
+
+  /**
+   * Make HTTP request to the given URI with a DELETE method.
+   *
+   * @param {string} uri
+   * @return {Promise}
+   */
+  delete(uri) {
+    return this.request('DELETE', uri);
+  },
+
+  /**
+   * Make HTTP request to the given URI with provided data.
+   *
+   * @param {string} method
+   * @param {string} uri
+   * @param {Object} query
+   * @param {Object} data
+   * @return {Promise}
+   */
+  request(method, uri, query = null, data = null) {
+    return new Promise((resolve, reject) => {
+      const req = request(method.toUpperCase(), uri);
+
+      if (query) req.query(query);
+
+      if (data) req.send(data);
+
+      req.end((err, res) => err ? reject(err) : resolve(res));
+    });
+  },
+};
